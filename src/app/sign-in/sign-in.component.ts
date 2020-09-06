c0f9ffea76b3dce8c79ad1d462991d61ae858e15
avinash0705 <avinash7may@gmail.com>
2020-09-06 13:54:25 +0530

    sign in page design and validation

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { User } from 'src/models/User';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -16,19 +17,19 @@ export class SignInComponent  {
 
   model = new User( 'a@b.com', '');
 
-
+  constructor(private router :Router){}
 
   onSubmit()
   { 
 
-     // we will send this data to backend  but now we are displaying the console only
+     // we will send this data to verify with the  backend  but now we are displaying the console only
 
     console.log(this.model);
 
    
 
 
-
+    
     this.newform();
   }
 
